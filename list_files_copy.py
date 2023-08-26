001a4afb9cb8bf70743563bb1a0e2432a9aed21f
huhreenee <hariniuiyer29@gmail.com>
2023-08-26 20:19:43 +0530

    added way to list files in subfolders

--- list_files_copy.py
+++ list_files_copy.py
@@ -1,3 +1,27 @@
+# import os
+# import pandas as pd
+# from time import time
+# import datetime
+
+# currentDT = datetime.datetime.now()
+# ts = currentDT.strftime("%Y_%m_%d_%H_%M_%S")
+# directory = input("Enter full path to list files:")
+# #directory = '/Users/harini/Documents/bts'
+# #file_list = [(file,os.path.join(directory, file)) for file in os.listdir(directory)]
+
+# # for top, dirs, files in os.walk(directory):
+# #     for file in files:
+# #         print(file, os.path.join(top, file))
+
+# # result = [(dir, os.path.join(top, dir)) for top, dirs, files in os.walk(directory) for dir in dirs]
+# result = [(file, os.path.join(top, file)) for top, dirs, files in os.walk(directory) for file in files]
+
+
+# # result = [(os.path.join(top,dir),files,os.path.join(top, files)) for top, dirs, files in os.walk(directory)]
+# df = pd.DataFrame(result, columns =['Name', 'Path'])
+# df = df.reset_index(drop=True)
+
+# df.to_excel("list_of_files_"+ts+".xlsx", index=False)
 import os
 import pandas as pd
 from time import time
@@ -6,23 +30,7 @@ import datetime
 currentDT = datetime.datetime.now()
 ts = currentDT.strftime("%Y_%m_%d_%H_%M_%S")
 directory = input("Enter full path to list files:")
-#file_list = [(file,os.path.join(directory, file)) for file in os.listdir(directory)]
-
-# for top, dirs, files in os.walk(directory):
-#     for dir in dirs:
-#         print(dir, os.path.join(top, dir))
-#     for file in files:
-#         print(file, os.path.join(top, file))
-
-result = [(dir, os.path.join(top, dir)) for top, dirs, files in os.walk(directory) for dir in dirs]
-result += [(file, os.path.join(top, file)) for top, dirs, files in os.walk(directory) for file in files]
-
+result = [(file, os.path.join(top, file)) for top, dirs, files in os.walk(directory) for file in files]
 df = pd.DataFrame(result, columns =['Name', 'Path'])
 
-def make_hyperlink(value):
-    return '=HYPERLINK("%s", "%s")' % (value, "Open" )
-
-#df['hyperlink'] = df['Path'].apply(make_hyperlink)
-
-df.to_excel("list_of_files_"+ts+".xlsx")
-
+df.to_excel("list_of_files_"+ts+".xlsx", index=False)
